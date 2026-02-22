a1f766a71719b2a1f990aaea2e6dd826a8b09715
yama.can <yama.can95@gmail.com>
2026-02-22 15:13:19 +0900

    [add] Add accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
 import { PrismaClient as WAuthPrismaClient } from '@atsuo-tech/w-auth-v2-prisma';
 import { PrismaClient as WSkillsPrismaClient } from '@atsuo-tech/w-skills-prisma';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 export const wAuthPrisma = new WAuthPrismaClient({
 	accelerateUrl: process.env.W_AUTH_PRISMA_DATABASE_URL!,
-});
+}).$extends(withAccelerate());
 
 export const wSkillsPrisma = new WSkillsPrismaClient({
 	accelerateUrl: process.env.W_SKILLS_PRISMA_DATABASE_URL!,
-});
+}).$extends(withAccelerate());
